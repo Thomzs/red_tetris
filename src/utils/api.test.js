eaaa0cbd4cd0ff285fca8a734af5b11a079cb165
Thomas MEYER <tmeyer@student.42.fr>
2022-09-14 15:40:52 +0200

    Go snow go

--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -7,102 +7,101 @@ import {checkRoomPassword, directRoomRequest, getRooms, requestCreateRoom} from 
 import "../setupTests";
 
 let server;
-let serverError;
 let response = [{id:1, room:'bla'}, {id:2, room:'bla'}, {id:3, room:'bla'}];
-let errorResponse = {message: 'test'};
+let message = {message: 'test'};
+let error = false;
 
-describe("api tests no Errors", () => {
+describe("api tests", () => {
     beforeAll((done) => {
         app.use(cors());
 
         app.get('/rooms', (req,res) => {
-            res.send(JSON.stringify(response));
+            if (error) {
+                res.status(500);
+                res.send(message);
+            } else {
+                res.send(JSON.stringify(response));
+            };
         });
         app.get('/askRoom', (req, res) => {
-           res.send(JSON.stringify(response));
+            if (error) {
+                res.status(500);
+                res.send(message);
+            } else {
+                res.send(JSON.stringify(response));
+            };
         });
         app.get('/createRoom', (req, res) => {
-            res.send(JSON.stringify(response));
+            if (error) {
+                res.status(500);
+                res.send(message);
+            } else {
+                res.send(JSON.stringify(response));
+            };
         });
         app.get('/directRequest?', (req, res) => {
-            res.send(JSON.stringify(response));
-        });
-        serverError = app.listen(8080, done);
-    });
-
-    afterAll((done) => {
-        serverError.close(done);
-    })
-
-    test('getRooms', () => {
-        getRooms().then((ret) => {
-            expect(ret).toEqual(response);
-        });
-    });
-
-    test('askRoom', () => {
-        checkRoomPassword('1', 'pass').then((ret) => {
-            expect(ret).toEqual(response);
-        })
-    });
-
-    test('requestCreateRoom', () => {
-        requestCreateRoom('name', 'normal', 'pass').then((ret) => {
-            expect(ret).toEqual(response);
-        })
-    });
-
-    test('directRoomRequest', () => {
-        directRoomRequest('name', 'pass').then((ret) => {
-            expect(ret).toEqual(response);
-        })
-    });
-});
-
-describe("api tests with Errors", () => {
-    beforeAll((done) => {
-        app.use(cors());
-
-        app.get('/rooms', (req,res) => {
-            res.status(500);
-            res.send(JSON.stringify(errorResponse));
-        });
-        app.get('/askRoom', (req, res) => {
-            res.status(500);
-            res.send(JSON.stringify(errorResponse));
-        });
-        app.get('/createRoom', (req, res) => {
-            res.status(500);
-            res.send(JSON.stringify(errorResponse));
-        });
-        app.get('/directRequest?', (req, res) => {
-            res.status(500);
-            res.send(JSON.stringify(errorResponse));
+            if (error) {
+                res.status(500);
+                res.send(message);
+            } else {
+                res.send(JSON.stringify(response));
+            };
         });
         server = app.listen(8080, done);
     });
 
+
     test('getRooms', () => {
+        error = false;
         getRooms().then((ret) => {
-            expect(ret).toEqual(errorResponse);
+            expect(ret).toEqual(response);
         });
+        error = true;
+        expect(getRooms()).toThrow(Error);
+        exect(getRooms()).toThrow(message.message);
+        // try {
+        //     getRooms().then();
+        // } catch (e) {
+        //     expect(e).toEqual(message);
+        // }
     });
 
-    test('askRoom', () => {
-        checkRoomPassword('1', 'pass').catch((ret) => {
-            expect(ret).toEqual(errorResponse);
-        })
-    });
-
-    test('requestCreateRoom', () => {
-        requestCreateRoom('name', 'normal', 'pass').catch((ret) => {
-            expect(ret).toEqual(errorResponse);
-        })
-    });
-
-    test('directRoomRequest', () => {
-        directRoomRequest('name', 'pass').catch((ret) => {
-            expect(ret).toEqual(errorResponse);
-        })
-    });
+    // test('askRoom', () => {
+    //     error = false;
+    //     checkRoomPassword('1', 'pass').then((ret) => {
+    //         expect(ret).toEqual(response);
+    //     });
+    //     error = true;
+    //     try {
+    //         checkRoomPassword('1', 'pass').then();
+    //     } catch (e) {
+    //         expect(e).toEqual(message);
+    //     }
+    // });
+    //
+    // test('requestCreateRoom', () => {
+    //     error = false;
+    //     requestCreateRoom('name', 'normal', 'pass').then((ret) => {
+    //         expect(ret).toEqual(response);
+    //     });
+    //     error = true;
+    //     try {
+    //         requestCreateRoom('name', 'normal', 'pass').then();
+    //     } catch (e) {
+    //         expect(e).toEqual(message);
+    //     }
+    // });
+    //
+    // test('directRoomRequest', () => {
+    //     error = false;
+    //     directRoomRequest('name', 'pass').then((ret) => {
+    //         expect(ret).toEqual(response);
+    //     });
+    //     error = true;
+    //     try {
+    //         directRoomRequest('name', 'pass').then();
+    //     } catch (e) {
+    //         expect(e).toEqual(message);
+    //     }
+    // });
 });
